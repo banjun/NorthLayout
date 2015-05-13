e3e4d1ca035e9fda6759ebc3bbdc78e1ca278135
banjun <banjun@gmail.com>
2015-05-13 22:50:24 +0900

    add fav and reply button to example for buttons with equal-width

--- Example/NorthLayout-ios/ViewController.swift
+++ Example/NorthLayout-ios/ViewController.swift
@@ -43,17 +43,36 @@ class ViewController: UIViewController {
         let textLabel = UILabel()
         textLabel.text = "Some text go here"
         
+        let favButton = UIButton.buttonWithType(.System) as! UIButton
+        favButton.setTitle("⭐️", forState: .Normal)
+        favButton.backgroundColor = UIColor(red: 0.17, green: 0.29, blue: 0.45, alpha: 1.0)
+        favButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        favButton.layer.cornerRadius = 4
+        favButton.clipsToBounds = true
+        
+        let replyButton = UIButton.buttonWithType(.System) as! UIButton
+        replyButton.setTitle("Reply", forState: .Normal)
+        replyButton.backgroundColor = favButton.backgroundColor
+        replyButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        replyButton.layer.cornerRadius = 4
+        replyButton.clipsToBounds = true
+        
         let autolayout = view.northLayoutFormat(["p": 8, "iconWidth": iconWidth], [
             "icon": iconView,
             "name": nameLabel,
             "date": dateLabel,
             "text": textLabel,
+            "fav": favButton,
+            "reply": replyButton,
             ])
         autolayout("H:|-p-[icon(==iconWidth)]-p-[name]-p-[date]-p-|")
         autolayout("H:|-p-[text]-p-|")
+        autolayout("H:|-p-[fav]-p-[reply(==fav)]-p-|")
         autolayout("V:|-p-[icon(==iconWidth)]-p-[text]")
         autolayout("V:|-p-[name(==icon)]")
         autolayout("V:|-p-[date]")
+        autolayout("V:[text]-p-[fav]")
+        autolayout("V:[text]-p-[reply]")
     }
 }
 
