5238bd816721272003c6b0a4244d75a99b8177f5
banjun <banjun@gmail.com>
2015-05-13 22:41:39 +0900

    more practical example layout

--- Example/NorthLayout-ios/ViewController.swift
+++ Example/NorthLayout-ios/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 import NorthLayout
 
 
+private func colorImage(color: UIColor) -> UIImage {
+    UIGraphicsBeginImageContext(CGSizeMake(1, 1))
+    color.set()
+    UIRectFill(CGRectMake(0, 0, 1, 1))
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return image
+}
+
+
 class ViewController: UIViewController {
     override func loadView() {
         super.loadView()
@@ -17,21 +27,33 @@ class ViewController: UIViewController {
         edgesForExtendedLayout = nil
         view.backgroundColor = UIColor.whiteColor()
         
+        let iconView = UIImageView(image: colorImage(UIColor(red: 0.63, green: 0.9, blue: 1, alpha: 1)))
+        let iconWidth = CGFloat(32)
+        iconView.layer.cornerRadius = iconWidth / 2
+        iconView.clipsToBounds = true
+        
         let nameLabel = UILabel()
         nameLabel.text = "Name"
-        nameLabel.backgroundColor = UIColor.grayColor()
+        
+        let dateLabel = UILabel()
+        dateLabel.text = "1 min ago"
+        dateLabel.font = UIFont.systemFontOfSize(12)
+        dateLabel.textColor = UIColor.lightGrayColor()
         
         let textLabel = UILabel()
-        textLabel.text = "Some text label"
-        textLabel.backgroundColor = UIColor.lightGrayColor()
+        textLabel.text = "Some text go here"
         
-        let autolayout = view.northLayoutFormat(["p": 8], [
+        let autolayout = view.northLayoutFormat(["p": 8, "iconWidth": iconWidth], [
+            "icon": iconView,
             "name": nameLabel,
+            "date": dateLabel,
             "text": textLabel,
             ])
-        autolayout("H:|-p-[name]-p-|")
+        autolayout("H:|-p-[icon(==iconWidth)]-p-[name]-p-[date]-p-|")
         autolayout("H:|-p-[text]-p-|")
-        autolayout("V:|-p-[name]-p-[text]")
+        autolayout("V:|-p-[icon(==iconWidth)]-p-[text]")
+        autolayout("V:|-p-[name(==icon)]")
+        autolayout("V:|-p-[date]")
     }
 }
 
