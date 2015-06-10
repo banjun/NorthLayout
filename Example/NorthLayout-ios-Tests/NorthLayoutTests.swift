5a66a689a7e972cd9f26cdd2c182d733d600e41f
banjun <banjun@gmail.com>
2015-06-10 14:48:32 +0900

    add ios test

--- Example/NorthLayout-ios-Tests/NorthLayoutTests.swift
+++ Example/NorthLayout-ios-Tests/NorthLayoutTests.swift
@@ -8,29 +8,40 @@
 
 import UIKit
 import XCTest
+import NorthLayout
 
 class NorthLayoutTests: XCTestCase {
+    let rootView: UIView = {
+        let window = UIWindow(frame: CGRectMake(0, 0, 320, 480))
+        let v = UIView(frame: window.bounds)
+        v.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        window.addSubview(v)
+        return v
+        }()
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test() {
+        let label = UILabel()
+        label.text = "label"
+        let field = UITextField()
+        field.text = "text field"
+        
+        let p = CGFloat(16)
+        let autolayout = rootView.northLayoutFormat(["p": p], [
+            "label": label,
+            "field": field,
+            ])
+        
+        autolayout("H:|-p-[label]-p-[field]-p-|")
+        autolayout("V:|-p-[label]")
+        autolayout("V:|-p-[field]")
+        
+        rootView.layoutIfNeeded()
+        
+        XCTAssertEqual(label.frame.origin.x, p)
+        XCTAssertEqual(field.frame.origin.x, CGRectGetMaxX(label.frame) + p)
+        XCTAssertEqual(CGRectGetMaxX(field.frame), rootView.frame.width - p)
+        
+        XCTAssertEqual(label.frame.origin.y, p)
+        XCTAssertEqual(field.frame.origin.y, p)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
