44ff3ceece6fc4dbdbe8d4a67b215ef076a8a104
banjun <banjun@gmail.com>
2015-09-19 11:18:19 +0900

    add tvos target

---
+++ Example/NorthLayout-tvos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NorthLayout-tvos
+//
+//  Created by BAN Jun on 9/19/15.
+//  Copyright © 2015 banjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
