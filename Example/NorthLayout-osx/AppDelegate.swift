8b0b03506d6b79f8c261f517606621b748f7425c
banjun <banjun@gmail.com>
2015-09-19 11:09:59 +0900

    update for Xcode 7 + osx

--- Example/NorthLayout-osx/AppDelegate.swift
+++ Example/NorthLayout-osx/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
-        let view = window.contentView
+        let view = window.contentView!
         
         let nameLabel = NSTextField()
         nameLabel.stringValue = "Name"
