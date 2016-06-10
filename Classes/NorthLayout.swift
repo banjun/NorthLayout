ea4add723abac18408b8e1fed5b026007b88b955
BAN Jun <banjun@gmail.com>
2016-06-10 16:33:24 +0900

    add MinView flexible spacer, not hug larger than default priority ui controls and not preventing window resize

--- Classes/NorthLayout.swift
+++ Classes/NorthLayout.swift
@@ -8,9 +8,43 @@
 #if os(iOS)
     import class UIKit.UIView
     typealias View = UIView
+    typealias Size = CGSize
+    typealias LayoutPriority = UILayoutPriority
+    typealias LayoutAxis = UILayoutConstraintAxis
+    extension View: LayoutPrioritizable {}
+
+    public final class MinView: UIView, MinLayoutable {
+        public init() {
+            super.init(frame: .zero)
+            setup()
+        }
+        public required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+        public override func intrinsicContentSize() -> CGSize {return .zero}
+    }
+
 #else
     import class AppKit.NSView
     typealias View = NSView
+    typealias Size = NSSize
+    typealias LayoutPriority = NSLayoutPriority
+    typealias LayoutAxis = NSLayoutConstraintOrientation
+    extension View: LayoutPrioritizable {
+        func setContentCompressionResistancePriority(priority: LayoutPriority, forAxis axis: LayoutAxis) {
+            setContentCompressionResistancePriority(priority, forOrientation: axis)
+        }
+        func setContentHuggingPriority(priority: LayoutPriority, forAxis axis: LayoutAxis) {
+            setContentHuggingPriority(priority, forOrientation: axis)
+        }
+    }
+
+    public final class MinView: NSView, MinLayoutable {
+        public init() {
+            super.init(frame: .zero)
+            setup()
+        }
+        public required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+        public override var intrinsicContentSize: NSSize {return .zero}
+    }
 #endif
 
 
@@ -64,3 +98,32 @@ extension View {
         }
     }
 #endif
+
+
+protocol LayoutPrioritizable {
+    func setContentCompressionResistancePriority(priority: LayoutPriority, forAxis axis: LayoutAxis)
+    func setContentHuggingPriority(priority: LayoutPriority, forAxis axis: LayoutAxis)
+}
+
+
+extension LayoutPriority {
+    static var required: LayoutPriority = 1000
+    static var high: LayoutPriority = 750
+    static var fitInWindow: LayoutPriority = 500 - 1 // = NSLayoutPriorityWindowSizeStayPut - 1
+    static var low: LayoutPriority = 250
+    static var fittingSize: LayoutPriority = 50
+}
+
+
+// common setup for MinView
+protocol MinLayoutable: LayoutPrioritizable {
+    func setup()
+}
+extension MinLayoutable {
+    func setup() {
+        setContentCompressionResistancePriority(LayoutPriority.fittingSize, forAxis: .Horizontal)
+        setContentCompressionResistancePriority(LayoutPriority.fittingSize, forAxis: .Vertical)
+        setContentHuggingPriority(LayoutPriority.fitInWindow, forAxis: .Horizontal)
+        setContentHuggingPriority(LayoutPriority.fitInWindow, forAxis: .Vertical)
+    }
+}
