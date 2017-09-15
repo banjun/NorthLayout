26a00a5920b1c822e2e9a8db185efb52f258329c
BAN Jun <banjun@gmail.com>
2017-09-15 15:18:54 +0900

    safe area replacing supports relations and priorities

--- Classes/NorthLayout.swift
+++ Classes/NorthLayout.swift
@@ -83,38 +83,77 @@ extension View {
             return { (format: String) in
                 let replacedFormat = NSMutableString(string: format)
 
-                func edgeConstraints(pattern: String) -> (view: AnyObject, constant: CGFloat)? {
+                func edgeConstraints(pattern: String) -> (view: UIView, constraint: (NSLayoutXAxisAnchor, NSLayoutXAxisAnchor) -> [NSLayoutConstraint])? {
                     let regex = try! NSRegularExpression(pattern: pattern)
                     return regex.firstMatch(in: String(replacedFormat), range: NSRange(location: 0, length: replacedFormat.length)).flatMap { result in
                         guard result.range.location != NSNotFound else { return nil }
 
                         let connection = result.range(withName: "connection")
-                        let metric = result.range(withName: "metric")
-                        let margin: CGFloat = {
-                            if metric.location != NSNotFound {
-                                // "|-...-[..." or "...]-...-|"
-                                let name = replacedFormat.substring(with: metric)
-                                return metrics[name] ?? Double(name).map {CGFloat($0)} ?? 0
+                        let predicateList = result.range(withName: "predicateList")
+                        var predicates: [(relation: NSLayoutRelation, margin: CGFloat, priority: LayoutPriority?)] = []
+                        if predicateList.location != NSNotFound {
+                            // "|-...-[..." or "...]-...-|"
+                            let predicatesText = replacedFormat.substring(with: predicateList)
+                            if let simplePredicate = (metrics[predicatesText] ?? Double(predicatesText).map {CGFloat($0)}) {
+                                predicates = [(.equal, simplePredicate, nil)]
                             } else {
-                                // "|[..." or "...]|"
-                                // "|-[..." or "...]-|"
-                                let systemSpacing = 8
-                                return CGFloat(connection.length == 0 ? 0 : systemSpacing)
+                                let predicatesTexts = predicatesText.trimmingCharacters(in: CharacterSet(charactersIn: "()")).split(separator: ",").map {$0.trimmingCharacters(in: .whitespaces)}
+                                // (<relation>)?(<objectOfPredicate>)(@<priority>)?
+                                let pat = try! NSRegularExpression(pattern: "(?<relation>(==|<=|>=))?(?<constant>[_0-9a-zA-Z]+)(@(?<priority>[_0-9a-zA-Z]+))?")
+                                predicates = predicatesTexts.flatMap { s in
+                                    pat.matches(in: s, range: NSRange(location: 0, length: NSString(string: s).length)).flatMap { result in
+                                        guard result.range.location != NSNotFound else { return nil }
+                                        let relationText = NSString(string: s).substring(with: result.range(withName: "relation"))
+                                        let constantText = NSString(string: s).substring(with: result.range(withName: "constant")) // metricName | number
+                                        let priorityText = result.range(withName: "priority").location == NSNotFound ? nil : NSString(string: s).substring(with: result.range(withName: "priority")) // metricName | number
+                                        let relation: NSLayoutRelation
+                                        switch relationText {
+                                        case "<=": relation = .lessThanOrEqual
+                                        case "==": relation = .equal
+                                        case ">=": relation = .greaterThanOrEqual
+                                        default: fatalError()
+                                        }
+                                        let margin = (metrics[constantText] ?? Double(constantText).map {CGFloat($0)})!
+                                        let priority = priorityText.map {LayoutPriority(rawValue: metrics[$0].map {Float($0)} ?? Float($0)!)}
+                                        return (relation, margin, priority)
+                                    }
+                                }
                             }
-                        }()
+                        } else {
+                            // "|[..." or "...]|"
+                            // "|-[..." or "...]-|"
+                            let systemSpacing = 8
+                            let margin = CGFloat(connection.length == 0 ? 0 : systemSpacing)
+                            predicates = [(.equal, margin, nil)]
+                        }
+
                         let edgeViewName = replacedFormat.substring(with: result.range(withName: "view"))
-                        guard let edgeView = views[edgeViewName] else { return nil }
+                        guard let edgeView = views[edgeViewName] as? UIView else { return nil }
 
                         replacedFormat.replaceCharacters(in: result.range(withName: "removed"), with: "")
-                        return (edgeView, margin)
+                        return (edgeView, { lhs, rhs in
+                            predicates.map { relation, margin, priority in
+                                let constraint: NSLayoutConstraint
+                                switch relation {
+                                case .lessThanOrEqual:
+                                    constraint = lhs.constraint(lessThanOrEqualTo: rhs, constant: margin)
+                                case .equal:
+                                    constraint = lhs.constraint(equalTo: rhs, constant: margin)
+                                case .greaterThanOrEqual:
+                                    constraint = lhs.constraint(greaterThanOrEqualTo: rhs, constant: margin)
+                                }
+                                _ = priority.map {constraint.priority = $0}
+                                return constraint
+                            }
+                        })
                     }
                 }
 
-                if let left = edgeConstraints(pattern: "^(H:)?(?<removed>\\|(?<connection>|-(?<metric>[_0-9a-zA-Z]+)-|-))\\[(?<view>[_0-9a-zA-Z]+)") {
-                    left.view.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: left.constant).isActive = true
+                if let left = edgeConstraints(pattern: "^(H:)?(?<removed>\\|(?<connection>|-(?<predicateList>[_0-9a-zA-Z(><=,@)]+)-|-))\\[(?<view>[_0-9a-zA-Z]+)") {
+                    left.constraint(left.view.leftAnchor, safeAreaLayoutGuide.leftAnchor).forEach {$0.isActive = true}
                 }
-                if let right = edgeConstraints(pattern: "^(H:)?.*\\[(?<view>[_0-9a-zA-Z]+).*\\](?<removed>(?<connection>|-(?<metric>[_0-9a-zA-Z]+)-|-)\\|)$") {
-                    right.view.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor, constant: -right.constant).isActive = true
+                if let right = edgeConstraints(pattern: "^(H:)?.*\\[(?<view>[_0-9a-zA-Z]+).*\\](?<removed>(?<connection>|-(?<predicateList>[_0-9a-zA-Z(><=,@)]+)-|-)\\|)$") {
+                    right.constraint(safeAreaLayoutGuide.rightAnchor, right.view.rightAnchor).forEach {$0.isActive = true}
                 }
 
                 autolayoutWithVerticalGuides(String(replacedFormat))
