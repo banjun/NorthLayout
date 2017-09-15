86005be7e8896bb821f86bd1d2682219079198c8
BAN Jun <banjun@gmail.com>
2017-09-15 13:42:21 +0900

    replace superview bounding constraints into safe area guides. currently supporting constant metrics == relation.

--- Classes/NorthLayout.swift
+++ Classes/NorthLayout.swift
@@ -60,7 +60,8 @@ extension View {
 #if os(iOS)
     extension UIViewController {
         /// autolayout by replacing vertical edges `|`...`|` to `topLayoutGuide` and `bottomLayoutGuide`
-        public func northLayoutFormat(_ metrics: [String: CGFloat], _ views: [String: AnyObject], options: NSLayoutFormatOptions = []) -> (String) -> Void {
+        public func northLayoutFormat(_ metrics: [String: CGFloat], _ views: [String: AnyObject], options: NSLayoutFormatOptions = [], useSafeArea: Bool = true) -> (String) -> Void {
+            guard let view = view else { fatalError() }
             guard view.enclosingScrollView == nil else {
                 // fallback to the view.northLayoutFormat because UIScrollView.contentSize is measured by its layout but not by the layout guides of this view controller
                 return view.northLayoutFormat(metrics, views, options: options)
@@ -70,10 +71,53 @@ extension View {
             vs["topLayoutGuide"] = topLayoutGuide
             vs["bottomLayoutGuide"] = bottomLayoutGuide
             let autolayout = view.northLayoutFormat(metrics, vs, options: options)
-            return { (format: String) in
+            let autolayoutWithVerticalGuides: (String) -> Void = { format in
                 autolayout(!format.hasPrefix("V:") ? format : format
                     .replacingOccurrences(of: "V:|", with: "V:[topLayoutGuide]")
                     .replacingOccurrences(of: "|", with: "[bottomLayoutGuide]"))
+            }
+
+            guard #available(iOS 11, tvOS 11, *), useSafeArea else { return autolayoutWithVerticalGuides }
+            let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+
+            return { (format: String) in
+                let replacedFormat = NSMutableString(string: format)
+
+                func edgeConstraints(pattern: String) -> (view: AnyObject, constant: CGFloat)? {
+                    let regex = try! NSRegularExpression(pattern: pattern)
+                    return regex.firstMatch(in: String(replacedFormat), range: NSRange(location: 0, length: replacedFormat.length)).flatMap { result in
+                        guard result.range.location != NSNotFound else { return nil }
+
+                        let connection = result.range(withName: "connection")
+                        let metric = result.range(withName: "metric")
+                        let margin: CGFloat = {
+                            if metric.location != NSNotFound {
+                                // "|-...-[..." or "...]-...-|"
+                                let name = replacedFormat.substring(with: metric)
+                                return metrics[name] ?? Double(name).map {CGFloat($0)} ?? 0
+                            } else {
+                                // "|[..." or "...]|"
+                                // "|-[..." or "...]-|"
+                                let systemSpacing = 8
+                                return CGFloat(connection.length == 0 ? 0 : systemSpacing)
+                            }
+                        }()
+                        let edgeViewName = replacedFormat.substring(with: result.range(withName: "view"))
+                        guard let edgeView = views[edgeViewName] else { return nil }
+
+                        replacedFormat.replaceCharacters(in: result.range(withName: "removed"), with: "")
+                        return (edgeView, margin)
+                    }
+                }
+
+                if let left = edgeConstraints(pattern: "^(H:)?(?<removed>\\|(?<connection>|-(?<metric>[_0-9a-zA-Z]+)-|-))\\[(?<view>[_0-9a-zA-Z]+)") {
+                    left.view.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: left.constant).isActive = true
+                }
+                if let right = edgeConstraints(pattern: "^(H:)?.*\\[(?<view>[_0-9a-zA-Z]+).*\\](?<removed>(?<connection>|-(?<metric>[_0-9a-zA-Z]+)-|-)\\|)$") {
+                    right.view.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor, constant: -right.constant).isActive = true
+                }
+
+                autolayoutWithVerticalGuides(String(replacedFormat))
             }
         }
     }
